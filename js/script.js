c20917f38709cd4602ffd3f1e70823adff404046
Keanneyi Xelhua <kxelhua@tywls.org>
2019-05-29 01:15:45 +0000

    stuff

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 /* global $ */
-
-console.log('hi');
-
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+$(function() {
+    
+    $("#weather").click(function(){
+        var zipCode = $("#zipCode").val();
+        var countryCode = $("#countryCode").val();
+        console.log(zipCode);
+        console.log(countryCode);
+        
+            $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ","+ countryCode + "&APPID=24bed9e2c3af6fb1b50671e367281625",
+                method: "GET",
+                success: function(response) {
+                // function runs if successful
+                console.log(response);
+                $("#main").append(response.weather[0].main);
+                
+                $("#description").append(response.weather[0].description);
+            
+                $("#humid").append(response.main.humidity);   
+              },
+            });
+           
+        });
 });
